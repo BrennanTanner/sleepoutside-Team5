cace7cd7116d69578220e328e8365b488796ec76
stewartrowley <stewy.rowley@gmail.com>
2022-10-01 10:07:49 -0600

    Team Activity 2 code

--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,3 +1,5 @@
+import { setLocalStorage } from './utils.js';
+
 export default class ProductDetails {
     constructor(productId, dataSource){
         this.productId = productId;
@@ -5,7 +7,9 @@ export default class ProductDetails {
         this.dataSource = dataSource;
     }
 
-    init() {
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        document.querySelector('main').innerHTML = this.renderProductDetails();
         // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
         // once we have the product details we can render out the HTML
         // once the HTML is rendered we can add a listener to Add to Cart button
@@ -15,10 +19,24 @@ export default class ProductDetails {
     }
 
     addToCart() {
-
+        setLocalStorage('so-cart', this.product);
     }
 
     renderProductDetails() {
-
+        return `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
+        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        <img
+          class="divider"
+          src="${this.product.Image}"
+          alt="${this.product.NameWithoutBrand}"
+        />
+        <p class="product-card__price">$${this.product.FinalPrice}</p>
+        <p class="product__color">${this.product.Colors[0].ColorName}</p>
+        <p class="product__description">
+        ${this.product.DescriptionHtmlSimple}
+        </p>
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+        </div></section>`;
     }
 }
