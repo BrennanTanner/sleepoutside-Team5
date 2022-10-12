1facd485ac44e045c43219deb115d98a613c74ae
stewartrowley <stewy.rowley@gmail.com>
2022-10-12 14:26:53 -0600

    updated dynamic website

--- src/js/utils.js
+++ src/js/utils.js
@@ -27,3 +27,12 @@ export function getParam() {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get('product');
 }
+
+
+export function renderListWithTemplate(template, parent, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
