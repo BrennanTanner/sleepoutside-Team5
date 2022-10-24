120309b980d99d6a1a85a00052c6a1fa66999892
Brennan Tanner <tan18010@byui.edu>
2022-10-24 15:05:40 -0600

    nondynamic pages+add breadcrumbs+add cart total

--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -33,7 +33,9 @@ export default class ProductDetails {
   }
 
   renderProductDetails() {
-    return `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
+    return `
+    <p>${this.product.Category}</p>
+    <section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
     <h2 class="divider">${this.product.NameWithoutBrand}</h2>
     <img
       class="divider"
